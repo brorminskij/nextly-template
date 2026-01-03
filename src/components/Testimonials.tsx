13845e975bd6e242d9ef871d8b3f31268987d6b0
Brorminskij <alexander.borminskij@gmail.com>
2026-01-03 13:29:08 +0100

    Cleaned testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,11 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Container } from "@/components/Container";
 import { useTranslation } from "@/i18n/TranslationProvider";
 import { ReactGoogleReviews } from "react-google-reviews";
 import "react-google-reviews/dist/index.css";
 
-import { UserIcon } from "@heroicons/react/24/solid";
 
 const featurableWidgetId = "9db39393-addf-4e2a-b47b-3a78fd72c91b";
 
@@ -69,48 +67,6 @@ export const Testimonials = () => {
     : localeItems;
 
   return (
-      <ReactGoogleReviews reviewCardStyle={{
-        //               height: 280,
-        // display: "flex",
-        // flexDirection: "column"
-      }} carouselStyle={{padding: 0}} carouselBtnStyle={{display: "none"}} showDots={false} layout="carousel" featurableId={featurableWidgetId} />
+      <ReactGoogleReviews carouselStyle={{padding: 0}} carouselBtnStyle={{display: "none"}} showDots={false} layout="carousel" featurableId={featurableWidgetId} />
   );
 };
-
-interface AvatarProps {
-  image: string;
-  name: string;
-  title: string;
-}
-
-function Avatar(props: Readonly<AvatarProps>) {
-  return (
-    <div
-      className="
-        flex items-center space-x-3 mt-8 
-        justify-center 
-        lg:justify-start
-      "
-    >
-      <div className="flex-shrink-0 w-14 h-14 rounded-full bg-gray-300 flex items-center justify-center dark:bg-gray-700">
-        <UserIcon className="w-10 h-10 text-gray-600 dark:text-gray-300" />
-      </div>
-
-      <div>
-        <div className="text-lg font-medium">{props.name}</div>
-        <div className="text-gray-600 dark:text-gray-400">{props.title}</div>
-      </div>
-    </div>
-  );
-}
-
-function Mark(props: { readonly children: React.ReactNode }) {
-  return (
-    <>
-      {" "}
-      <mark className="text-emerald-800 bg-emerald-100 rounded-md ring-emerald-100 ring-4 dark:ring-emerald-900 dark:bg-emerald-900 dark:text-emerald-200">
-        {props.children}
-      </mark>{" "}
-    </>
-  );
-}
